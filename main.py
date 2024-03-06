32d5ae17f96fbd94274afba5b27a2a50dd507ac8
julioorozco05 <julio11orozco09@gmail.com>
2024-03-06 15:00:32 -0600

    funcion de resta

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def suma(num_1: int, num_2: int) -> int:
     return num_1 + num_2
 
+def resta(num_1: int, num_2: int) -> int:
+    return num_1 - num_2
+
 if __name__ == "_main__":
     print(suma(num_1=5, num_2=10))
+    print(resta(num_1=10, num_2=8))
